76ac0ea3a37e894b80c2906e04b65263a59a360c
Yohan Vinu <yohanvvinu@gmail.com>
2023-04-30 15:52:13 +0530

    Implement GUI from PySimpleGUI

--- client.py
+++ client.py
@@ -1,37 +1,92 @@
+import PySimpleGUI as sg
 import socket
 import threading
 
-nickname = input("Choose your nickname: ")
+sg.theme("DarkTeal5")
+sg.set_options(font=("Helvetica", 13))
+layout = [
+    [sg.Text("Name:"), sg.InputText(key="-NAME-", size=(30, 1)),
+     sg.Button("Connect", key="-CONNECT-", size=(15, 1)),
+     sg.Button("Stop", key="-STOP1-", size=(15, 1))],
+    [sg.Multiline(key="-DISPLAY-", size=(70, 23), disabled=True,
+                  autoscroll=True)],
+    [sg.InputText(key="-MESSAGE-", size=(55, 1)),
+     sg.Button("Send", key="-SEND-", bind_return_key=True, size=(15, 1))],
+]
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 1234))
+window = sg.Window("Client", layout, size=(600, 425))
+
+username = ""
+client = None
+HOST_ADDR = "127.0.0.1"
+HOST_PORT = 8080
+msgs = []
 
 
-def receive():
+def connect():
+    global username, client
+    if len(values["-NAME-"]) < 1:
+        sg.popup_error("Enter a valid name.")
+    else:
+        username = values["-NAME-"]
+        connect_to_server(username)
+
+
+def connect_to_server(name):
+    global client, HOST_PORT, HOST_ADDR
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST_ADDR, HOST_PORT))
+        client.send(name.encode())
+
+        window["-NAME-"].update(disabled=True)
+        window["-CONNECT-"].update(disabled=True)
+        window["-MESSAGE-"].update(disabled=False)
+
+        thread = threading.Thread(target=receive_message_from_server,
+                                  args=(client,))
+        thread.start()
+    except Exception as e:
+        sg.popup_error(
+            "Cannot connect to host: " + HOST_ADDR + " on port: " + str(
+                HOST_PORT) + ". Server may be unavailable. Try again later.")
+
+
+def receive_message_from_server(sck):
     while True:
-        try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
-            else:
-                print(message)
-        except:
-            # Close Connection When Error
-            print("An error occured!")
-            client.close()
+        from_server = sck.recv(4096).decode()
+
+        if not from_server:
             break
 
+        if '->' in from_server:
+            from_server = from_server.replace('->', ' -> ')
 
-def write():
-    while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        msgs.append(from_server)
+        text = "\n".join(msgs)
+        window["-DISPLAY-"].update(f'{text}\n')
+
+    sck.close()
+    window.close()
 
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+while True:
+    event, values = window.read()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == "-CONNECT-":
+        connect()
+    elif event == "-STOP1-":
+        exit()
+    elif event == "-SEND-":
+        msg = values["-MESSAGE-"].replace('\n', '')
+        if msg:
+            client.send(msg.encode())
+            msgs.append('You -> '+msg)
+            text = "\n".join(msgs)
+            window["-DISPLAY-"].update(f'{text}\n')
+            window["-MESSAGE-"].update(value="")
+            window["-MESSAGE-"].set_focus()
+
+window.close()
--- server.py
+++ server.py
@@ -1,59 +1,150 @@
+import PySimpleGUI as sg
 import socket
 import threading
 
-host = '127.0.0.1'
-port = 1234
+sg.theme("DarkTeal5")
+sg.set_options(font=("Helvetica", 13))
+layout = [[sg.Button("Connect", key="-CONNECT-", size=(30, 1)),
+           sg.Button("Stop", key="-STOP-", disabled=True, size=(30, 1))],
+          [sg.Text("Host: X.X.X.X", key="-HOST-", size=(30, 1)),
+           sg.Text("Port: X.X.X.X", key="-PORT-", size=(30, 1))],
+          [sg.Text("Client List")], [sg.Multiline(size=(60, 15),
+                                                  key="-CLIENTS-",
+                                                  disabled=True)]]
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
+window = sg.Window("Server", layout, size=(400, 275))
 
+server = None
+HOST_ADDR = "127.0.0.1"
+HOST_PORT = 8080
+client_name = " "
 clients = []
-nicknames = []
+clients_names = []
 
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+def start_server():
+    global server, HOST_ADDR, HOST_PORT, window
+    window["-CONNECT-"].update(disabled=True)
+    window["-STOP-"].update(disabled=False)
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server.bind((HOST_ADDR, HOST_PORT))
+    server.listen(5)
+
+    thread = threading.Thread(target=accept_clients, args=(server, " "))
+    thread.start()
+
+    window["-HOST-"].update("Host: " + HOST_ADDR)
+    window["-PORT-"].update("Port: " + str(HOST_PORT))
 
 
-def handle(client):
+def stop_server():
+    global server, clients, clients_names, window
+    window["-CONNECT-"].update(disabled=False)
+    window["-STOP-"].update(disabled=True)
+
+    for c in clients:
+        c.close()
+
+    clients.clear()
+    clients_names.clear()
+
+    if server:
+        server.close()
+        server = None
+    exit()
+
+
+def accept_clients(the_server, y):
+    global clients, window
     while True:
-        try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
-            break
-
-
-def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
-
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
+        client, addr = the_server.accept()
         clients.append(client)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=send_receive_client_message,
+                                  args=(client, addr))
         thread.start()
 
 
-receive()
+def send_receive_client_message(client_connection, client_ip_addr):
+    global server, client_name, clients, clients_names, window
+    client_msg = " "
+
+    client_name = client_connection.recv(4096).decode()
+    welcome_msg = "Welcome " + client_name + "! Press stop to quit." + ('\n' if len(clients_names) == 0 else '')
+    client_connection.send(welcome_msg.encode())
+
+    if len(clients_names) != 0:
+        if len(clients_names) == 1:
+            allclients_msg = '\n'+clients_names[0] + ' is in this chat.\n'
+        elif len(clients_names) == 2:
+            allclients_msg = '\n'+' & '.join(clients_names)
+            allclients_msg = allclients_msg + ' are in this chat.\n'
+        else:
+            allclients_msg = '\n'+', '.join(clients_names[:len(clients_names) - 2])
+            allclients_msg = allclients_msg + '& ' + \
+                             clients_names[len(clients_names) - 1] + \
+                             ' are in this chat.\n'
+
+        client_connection.send(allclients_msg.encode())
+
+    clients_names.append(client_name)
+
+    update_client_names_display(clients_names)
+
+    for client in clients:
+        if client != client_connection:
+            server_msg = client_name + ' has joined the chat!'
+            client.send(server_msg.encode())
+
+    while True:
+        data = client_connection.recv(4096).decode()
+
+        if not data:
+            break
+
+        client_msg = data
+
+        idx = get_client_index(clients, client_connection)
+        sending_client_name = clients_names[idx]
+
+        for c in clients:
+            if c != client_connection:
+                server_msg = str(sending_client_name + "->" + client_msg)
+                c.send(server_msg.encode())
+
+    idx = get_client_index(clients, client_connection)
+    del clients_names[idx]
+    del clients[idx]
+    server_msg = "BYE!"
+    client_connection.send(server_msg.encode())
+    client_connection.close()
+
+    update_client_names_display(clients_names)
+
+
+def get_client_index(client_list, curr_client):
+    idx = 0
+    for conn in client_list:
+        if conn == curr_client:
+            break
+        idx = idx + 1
+
+    return idx
+
+
+def update_client_names_display(name_list):
+    window["-CLIENTS-"].update("\n".join(name_list))
+
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    if event == "-CONNECT-":
+        start_server()
+    elif event == "-STOP-":
+        stop_server()
+
+window.close()
